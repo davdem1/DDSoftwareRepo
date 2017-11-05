1332a33c6a41fef4a3e1a2219131d670d9e93b67
David Demers <daviddemers@verizon.net>
2017-11-04 20:34:39 -0400

    Test Again

---
+++ ddSoftware/ddSoftware/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ddSoftware.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "A long history with technology";
+
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            ViewBag.Message = "Here to Serve";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "DD Software";
+
+            return View();
+        }
+    }
+}
